514c6f5e2909911c8c9cbf6a8993fe9cb6053fc8
Meredith Ellington <34221120+mgellington@users.noreply.github.com>
2020-02-25 23:33:35 +0000

    committed after one (1) beer

---
+++ Loc.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+// List of Classes
+public class Loc {
+	private ArrayList<Class> classes;
+	
+	public Loc() {
+		this.classes = new ArrayList<Class>();
+	}
+	
+	public ArrayList<Class> getClasses() {
+		return this.classes;
+	}
+	
+	public void add(Class c) {
+		this.classes.add(c);
+	}
+	
+	public void remove(Class c) {
+		this.classes.remove(c);
+	}
+	
+	public Class find(int id) {
+		Class found = null;
+		for (Class c : classes) {
+			if (c.getClassID() == id) {
+				found = c;
+				break;
+			}
+		}
+		return found;
+	}
+	
+	public void print() {
+		for (Class c : classes) {
+			System.out.println(c);
+		}
+	}
+
+}
