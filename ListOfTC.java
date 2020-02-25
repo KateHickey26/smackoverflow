84736423d9d6c91b030d0691ecd2c79ec6df057d
kate hickey <kate.hickey26@gmail.com>
2020-02-25 22:27:32 +0000

    updates to ListOfTC

--- ListOfTC.java
+++ ListOfTC.java
@@ -1,41 +1,43 @@
+import java.util.ArrayList;
+
 //import java.rmi.registry.LocateRegistry;
 
 public class ListOfTC{
     
     protected String courseID;
-    protected ArrayList<TrainingCourse> ListOfTC;
+    protected ArrayList<TrainingCourse> listOfTC;
     protected int numOfCourses;
 
     // list constructor
     // passed an int, for number of indicies?
-    listOfTC(int numOfCourses){
-        listofTC = new ArrayList<TrainingCourse>(numOfCourses);
+    ListOfTC(int numOfCourses) {
+        listOfTC = new ArrayList<TrainingCourse>(numOfCourses);
     }
 
 
     // adds a training course to the array list
     protected void addCourse(TrainingCourse trainingCourse){
-        for(int i = 0; i<ListOfTC.size(); i++){
-            if(ListOfTC[i]==null){
-                listOfTC[i] = trainingCourse;
-                break;
+        for(int i = 0; i<listOfTC.size(); i++){
+            if(listOfTC.get(i)==null){
+                listOfTC.add(trainingCourse);
+                //break; 
             }
         }
     }
 
     // removes a training course from the array list
     public void removeCourse(TrainingCourse trainingCourse){
-        for(int i = 0; i<listOfTC.size(); i++){
-            if(listOfTC[i]==trainingCourse){
-                listOfTC[i]= null;
-                break;
+        for(int i = 0; i< listOfTC.size(); i++) {
+            if (listOfTC.get(i) == trainingCourse) {
+                listOfTC.remove(i);
+                //break;
             }
         }
     }
 
     // getters
-    public <String> getListOfTC(){
-        return ListOfTC;
+    public ArrayList<TrainingCourse> getListOfTC() {
+        return listOfTC;
     }
 
     public String getcourseID(){
